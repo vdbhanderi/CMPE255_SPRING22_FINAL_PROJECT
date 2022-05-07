123e93f571c2d6a40e7e3c7eba671630d92c2806
JJLZ21 <jializhu2016@gmail.com>
2022-05-06 22:26:23 -0700

    model.py

---
+++ ml/model.py
@@ -0,0 +1,49 @@
+import json
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+class textCorrectionModel:
+    def __init__(self) -> None:
+        self.max_length = 74
+        self.model = None
+        self._loadModel()
+
+    def _loadModel(self) -> None:
+        self.model = keras.models.load_model(
+            'char_level_accuracy_84_3lstm_rnn', compile=False)
+
+    def deTokenize(self, text_tokenizer, prediction):
+        index_to_words = {id: word for word,
+                          id in text_tokenizer.word_index.items()}
+        index_to_words[0] = ''
+        pre_index = np.argmax(prediction, 1)
+        return ''.join(index_to_words[word] for word in pre_index)
+
+    def predict(self):
+        test_string = ["wadeye residents eskeing return to outstaitosn"]
+        with open('g_text_tokenizer.json') as tokenizer:
+            tokenizer_data = json.load(tokenizer)
+            g_text_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(
+                tokenizer_data)
+
+        test_sample = g_text_tokenizer.texts_to_sequences(test_string)
+        test_sample = pad_sequences(
+            test_sample, maxlen=74, padding='post')
+
+        prediction = self.model.predict(test_sample)
+        with open('text_tokenizer.json') as tokenizer:
+            tokenizer_data = json.load(tokenizer)
+            text_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(
+                tokenizer_data)
+        detoken_prediction = self.deTokenize(text_tokenizer, prediction)
+
+        return detoken_prediction
+
+
+model = textCorrectionModel()
+print(model.predict())
