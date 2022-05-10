624b01410c5d9e841eb1e8981d3e27f114c59437
Kanupriya Agrawal <98919191+Kanupriya94@users.noreply.github.com>
2022-05-10 11:38:13 -0700

    Update request.py

--- request.py
+++ request.py
@@ -3,7 +3,6 @@ import requests
 from ml.model import textCorrectionModel
 
 url = 'http://localhost:4000/results'
-# r = requests.post(url,json={'rate':5, 'sales_in_first_month':200, 'sales_in_second_month':400})
 r = requests.post(url, json={'sentence': "hello my name is uolive"})
 
 print(r.json())
